7796acb6889e5bdd3163ee4a15def0b5934acdaf
TinGuanTou Sam <81403469+Chssam@users.noreply.github.com>
2026-06-05 13:40:11 +0800

    Delete src directory

--- src/data/shared.rs
+++
@@ -1,7 +0,0 @@
-use bevy_derive::*;
-use bevy_ecs::prelude::*;
-use bevy_reflect::Reflect;
-
-#[derive(Reflect, Component, Default, Debug, Deref, DerefMut)]
-#[reflect(Component)]
-pub struct Description(pub String);
